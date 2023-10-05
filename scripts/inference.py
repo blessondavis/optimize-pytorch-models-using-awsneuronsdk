d983f4f566d12b63deb90793248b50e2b4726f23
blessondavis <94608413+blessondavis@users.noreply.github.com>
2023-10-05 15:12:34 +0530

    create inference.py for the code

---
+++ scripts/inference.py
@@ -0,0 +1,60 @@
+import os
+import json 
+import torch
+# import tensorflow if any issue related to protobuf format. 
+import torch.neuron
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoConfig
+
+JSON_CONTENT_TYPE = 'application/json'
+
+
+def model_fn(model_dir):
+    """This function loads the model and the tokenizer from the model directory"""
+    
+    tokenizer_init = AutoTokenizer.from_pretrained("bert-base-cased-finetuned-mrpc")
+    model_file =os.path.join(model_dir, 'neuron_compiled_model.pt')
+    model_neuron = torch.jit.load(model_file)
+    return (model_neuron, tokenizer_init)
+
+
+def input_fn(serialized_input_data, content_type=JSON_CONTENT_TYPE):
+    """Converts to the input json data and sends it to the predict_fn()"""
+    
+    if content_type == JSON_CONTENT_TYPE:
+        input_data = json.loads(serialized_input_data)
+        return input_data
+
+    else:
+        raise Exception('Requested unsupported ContentType in Accept: ' + content_type)
+        return
+
+
+def predict_fn(input_data, models):
+    """Loads the input data and model to make predictions. All custom logic before and after the prediction
+     also be can be done in this part."""
+    
+    model_bert, tokenizer = models
+    sequence_0 = input_data[0] 
+    sequence_1 = input_data[1]
+    
+    max_length=128
+    paraphrase = tokenizer.encode_plus(sequence_0, sequence_1, max_length=max_length, padding='max_length', truncation=True, return_tensors="pt")
+    # Convert example inputs to a format that is compatible with TorchScript tracing
+    example_inputs_paraphrase = paraphrase['input_ids'], paraphrase['attention_mask'], paraphrase['token_type_ids']  
+
+    # Verify the TorchScript works on example inputs
+    paraphrase_classification_logits_neuron = model_bert(*example_inputs_paraphrase)
+    classes = ['not paraphrase', 'paraphrase']
+    paraphrase_prediction = paraphrase_classification_logits_neuron[0][0].argmax().item()
+    out_str = 'BERT says that "{}" and "{}" are {}'.format(sequence_0, sequence_1, classes[paraphrase_prediction])
+    
+    return out_str
+
+def output_fn(prediction_output, accept=JSON_CONTENT_TYPE):
+    """Converts the outputs of the predict_fn() in to json format"""
+    
+    if accept == JSON_CONTENT_TYPE:
+        return json.dumps(prediction_output), accept
+
+    raise Exception('Requested unsupported ContentType in Accept: ' + accept)
+
